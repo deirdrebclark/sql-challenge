9219c6fd7aaa235be521ce47f9388248ffeeff06
deirdrebclark <deirdrebclark@gmail.com>
2020-02-12 13:23:01 -0500

    Initial commit

---
+++ EmployeeSQL/schemata.sql
@@ -0,0 +1,85 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/6yEfi6
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Employee" (
+    "emp_no" integer   NOT NULL,
+    "first_name" varchar(60)   NOT NULL,
+    "last_name" varchar(75)   NOT NULL,
+    "dob" date   NOT NULL,
+    "gender" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employee" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Title" (
+    "title_id" serial   NOT NULL,
+    "emp_no" integer   NOT NULL,
+    "title" varchar(50)   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+    CONSTRAINT "pk_Title" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "Salery" (
+    "salary_id" serial   NOT NULL,
+    "emp_no" integer   NOT NULL,
+    "salary" float   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+    CONSTRAINT "pk_Salery" PRIMARY KEY (
+        "salary_id"
+     )
+);
+
+CREATE TABLE "Dept_Manager" (
+    "dept_no" integer   NOT NULL,
+    "emp_no" integer   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+    CONSTRAINT "pk_Dept_Manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "Department" (
+    "dept_no" integer   NOT NULL,
+    "dept_name" varchar(150)   NOT NULL,
+    CONSTRAINT "pk_Department" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Dept_Emp" (
+    "emp_no" integer   NOT NULL,
+    "dept_no" integer   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+    CONSTRAINT "pk_Dept_Emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+ALTER TABLE "Title" ADD CONSTRAINT "fk_Title_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+ALTER TABLE "Salery" ADD CONSTRAINT "fk_Salery_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Department" ("dept_no");
+
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employee" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Department" ("dept_no");
+
